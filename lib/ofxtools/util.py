fa66467af104d3c9f7dc96d63e2426e111ee0507
leandrost <leandro.s.thimoteo@gmail.com>
2015-09-17 18:48:48 -0300

    Fix parser error when OFX contains a self closed tag

--- lib/ofxtools/util.py
+++ lib/ofxtools/util.py
@@ -3,6 +3,6 @@ import re
 
 def strip_empty_tags(ofx):
     """Strips open/close tags that have no content."""
-    strip_search = '<(?P<tag>[^>]+)>\s*</(?P=tag)>'
+    strip_search = '(<(?P<tag>[^>]+)>\s*</(?P=tag)>|<(?P<closed_tag>[^>]+)/>)'
     return re.sub(strip_search, '', ofx)
 
